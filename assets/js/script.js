f9e18ef27c11d812a0ac28ea20b02d9bed2e78d1
Sunil Sowrirajan <146518538+suniltechs@users.noreply.github.com>
2025-04-21 20:14:43 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,7 +74,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "web designing", "web development"],
+    strings: ["MERN Stack Development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
